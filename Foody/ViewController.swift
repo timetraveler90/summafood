b6e6c7421e1fb726c1783b6779e6c53a4fad5139
Petar Stanisic <petar.stanisic@summa.io>
2019-08-27 12:36:27 +0200

    Initial Commit

---
+++ Foody/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by Petar Stanisic on 8/27/19.
+//  Copyright © 2019 Petar Stanisic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
