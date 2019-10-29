2851446ab0a7d114807c4800b594f55319a253fa
Petar Stanisic <petar.stanisic@summa.io>
2019-10-29 10:41:55 +0100

    Settings tab completed UI wise

---
+++ Foody/SettingsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsCollectionViewCell.swift
+//  Foody
+//
+//  Created by Petar Stanisic on 10/29/19.
+//  Copyright © 2019 Petar Stanisic. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCollectionViewCell: UICollectionViewCell {
+    
+}
--- Foody/SettingsViewController.swift
+++ Foody/SettingsViewController.swift
@@ -8,10 +8,42 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDataSource, UICollectionViewDelegate {
 
-    override func viewDidLoad() {
+	@IBOutlet weak var tableView: UITableView!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
+		tableView.tableFooterView = UIView(frame: .zero)
     }
+
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsTableViewCell") as! SettingsTableViewCell
+		return cell
+	}
+
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 10
+	}
+
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SettingsCollectionViewCell", for: indexPath) as! SettingsCollectionViewCell
+		return cell
+	}
+
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+
+	func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return "Select favorite food"
+	}
+
+
+
 
 }
