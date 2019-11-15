61b7606886d662cef67e36469f1c290d226071aa
Petar Stanisic <petar.stanisic@summa.io>
2019-11-15 09:09:51 +0100

    Menu parsing fixed

--- Foody/MenuViewController.swift
+++ Foody/MenuViewController.swift
@@ -11,6 +11,40 @@ import UIKit
 class MenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDelegate, UICollectionViewDataSource {
 
 	@IBOutlet weak var tableView: UITableView!
+	let userId = UserDefaults.standard.string(forKey: "userID") ?? ""
+	private var availableFood = [Order]()
+
+	struct Order: Codable {
+		let permission: String
+		let check: Int
+		let availableFood: AvailableFood
+
+		enum CodingKeys: String, CodingKey {
+			case permission, check
+			case availableFood = "available_food"
+		}
+	}
+
+	// MARK: - AvailableFood
+	struct AvailableFood: Codable {
+		let wednesday, tuesday, thursday, monday: [Day]
+		let friday: [Day]
+
+		enum CodingKeys: String, CodingKey {
+			case wednesday = "Wednesday"
+			case tuesday = "Tuesday"
+			case thursday = "Thursday"
+			case monday = "Monday"
+			case friday = "Friday"
+		}
+	}
+
+	// MARK: - Day
+	struct Day: Codable {
+		let name: String
+		let id: Int
+	}
+
 
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return 1
@@ -54,7 +88,34 @@ class MenuViewController: UIViewController, UITableViewDelegate, UITableViewData
     override func viewDidLoad() {
         super.viewDidLoad()
 		tableView.separatorStyle = .none
+		fetchJSON()
     }
+
+
+	fileprivate func fetchJSON() {
+		let urlString = "http://uc-dev.voiceworks.com:4000/external/available_food/\(userId)"
+		guard let url = URL(string: urlString) else { return }
+
+		URLSession.shared.dataTask(with: url) { (data, _, err) in
+			DispatchQueue.main.async {
+				if let err = err {
+					print("Failed to get data from URL: ", err)
+					return
+				}
+
+				guard let data = data else { return }
+				let decoder = JSONDecoder()
+
+				do {
+					self.availableFood = try [decoder.decode(Order.self, from: data)]
+					print(self.availableFood)
+				} catch let jsonErr {
+					print("Failed to decode: ", jsonErr)
+				}
+
+			}
+		}.resume()
+	}
 
 
 
