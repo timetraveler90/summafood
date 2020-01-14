e39d8f4411d2b5f392d45baa08f6fffee0f0a464
Petar Stanisic <petar.stanisic@summa.io>
2020-01-14 23:42:45 +0100

    Fixed the deselection of cells on settings screen

--- Foody/SettingsViewController.swift
+++ Foody/SettingsViewController.swift
@@ -59,7 +59,7 @@ class SettingsViewController: UIViewController, UICollectionViewDataSource, UICo
 
         if (model.favoriteFood.contains(food)) {
             cell.contentView.layer.borderWidth = 10
-			cell.contentView.layer.borderColor = #colorLiteral(red: 0.6588235294, green: 0.9294117647, blue: 0.9176470588, alpha: 1)
+			cell.contentView.layer.borderColor = #colorLiteral(red: 0.9960784314, green: 0.8392156863, blue: 0.8901960784, alpha: 1)
         } else {
             cell.contentView.layer.borderWidth = 0
         }
